52de0f7b2314b232e1ba606273316c0456fe3c66
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2025-03-02 15:48:37 -0800

    made the container component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import  Board  from "./components/Board";
+import Container from './components/Container';
 
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
     <>
      <div>
 
-        <Board />
+        <Container />
       
      </div>
     </>
---
+++ src/components/Container.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Board from './Board';
+
+const Container = () => {
+    return (
+        <div>
+            <h1>Rock Paper Scissors Game</h1>
+            <Board />
+        </div>
+    );
+};
+
+export default Container;
