9da347375c41a11863e44a2e39b5ee4f11c392b0
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2025-03-03 17:05:17 +0300

    Update Console.jsx
    added functionality to the buttons

--- src/components/Console.jsx
+++ src/components/Console.jsx
@@ -6,15 +6,22 @@ const Console = () => {
 const source = "../../images/";
 const images = [ "icon-paper.svg","icon-rock.svg", "icon-scissors.svg"];
 
+const handleClick = (value) => {
+    console.log(value);
+};
+
 return (
     <div className="console-container">
         <div className="console">
             <div className="console-body">
-                {images.map((image, index) => (
-                    <button key={index} className={`console-button ${image.split('-')[1].split('.')[0]}`}>
-                        <img src={`${source}${image}`} alt={image.split('-')[1]} />
-                    </button>
-                ))}
+                {images.map((image, index) => {
+                    const value = image.split('-')[1].split('.')[0];
+                    return (
+                        <button key={index} className={`console-button ${value}`} onClick={() => handleClick(value)}>
+                            <img src={`${source}${image}`} alt={value} />
+                        </button>
+                    );
+                })}
             </div>
         </div>
     </div>
