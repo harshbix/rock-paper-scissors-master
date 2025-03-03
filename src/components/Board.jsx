428951f7a0f88fc534ac946bfc873b6aa2c28b58
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2025-03-02 16:14:37 -0800

    simple improvement on the first component

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,17 +1,22 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function Board() {
-    return (
-        <>
-        <div className="board-container">
-                This is the board
+  return (
+    <>
+      <div className="board-container board-2 p-4">
+        <div className="items">
+         <span>ROCK</span> 
+        <span>PAPER</span>  
+          <span>SCISSORS</span>  
         </div>
-        
-        </>
-    );
-  }
-  
+      <div className="score">
+        <h4>SCORE</h4>
+        <h1>12</h1>
+      </div>
+      </div>
+    </>
+  );
+}
 
-
-  export default Board;
+export default Board;
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -5,7 +5,6 @@ import '../App.css';
 const Container = () => {
     return (
         <div className='cont-container'> 
-            <h1>Rock Paper Scissors Game</h1>
             <Board />
         </div>
     );
