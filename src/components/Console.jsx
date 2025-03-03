281b576937d2e55fe562fdec5522fd5dab4b8087
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2025-03-03 07:04:08 +0300

    Update Console.jsx
    loaded the options

--- src/components/Console.jsx
+++ src/components/Console.jsx
@@ -1,15 +1,24 @@
 import React from "react";
+import '../App.css';
 
 const Console = () => {
-  return (
+
+const source = "../../images/";
+const images = [ "icon-paper.svg","icon-rock.svg", "icon-scissors.svg"];
+
+return (
     <div className="console-container">
-      <div className="console">
-        <div className="console-body">
-          <p>Choose an option to start the game</p>
+        <div className="console">
+            <div className="console-body">
+                {images.map((image, index) => (
+                    <button key={index} className="console-button">
+                        <img src={`${source}${image}`} alt={image.split('-')[1]} />
+                    </button>
+                ))}
+            </div>
         </div>
-      </div>
     </div>
-  );
+);
 };
 
 export default Console;
