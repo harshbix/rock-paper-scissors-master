a0f652e771c3b62fa60802c3a22f29621aa13e56
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2025-03-03 07:24:37 +0300

    added the three buttons

--- src/components/Console.jsx
+++ src/components/Console.jsx
@@ -11,7 +11,7 @@ return (
         <div className="console">
             <div className="console-body">
                 {images.map((image, index) => (
-                    <button key={index} className="console-button">
+                    <button key={index} className={`console-button ${image.split('-')[1].split('.')[0]}`}>
                         <img src={`${source}${image}`} alt={image.split('-')[1]} />
                     </button>
                 ))}
