9efffa674812f1344bb0623a3356fcd87ea39c4d
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2025-03-03 06:49:51 +0300

    new component the console
    this is where games are actually played

---
+++ src/components/Console.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Console = () => {
+  return (
+    <div className="console-container">
+      <div className="console">
+        <div className="console-body">
+          <p>Choose an option to start the game</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Console;
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Board from './Board';
 import '../App.css';
+import Console from './console';
 
 const Container = () => {
     return (
         <div className='cont-container'> 
          <h1 className='text-center p-2'>Rock Paper Scissors Game</h1>
             <Board />
+            <Console />
         </div>
     );
 };
