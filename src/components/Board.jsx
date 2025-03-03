92a5396980667dd3176cfcfd440f80ce75939edb
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2025-03-03 06:25:59 +0300

    Update Board.jsx
    more refined code

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,14 +2,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function Board() {
+  const items = ["ROCK", "PAPER", "SCISSORS"];
   const score = 17;
   return (
     <>
       <div className="board-container p-3">
         <div className="items">
-          <span>ROCK</span>
-          <span>PAPER</span>
-          <span>SCISSORS</span>
+          <span>{items[0]}</span>
+          <span>{items[1]}</span>
+          <span>{items[2]}</span>
         </div>
         <div className="all">
           <div className="score">
