6067472a8f35247e5952fea8de870cc8f23bc23f
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2025-03-06 22:08:56 +0300

    Update Container.jsx
    added props for wins

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from './Board';
 import '../App.css';
 import Console from './console';
 
 const Container = () => {
+    const [score, setScore] = useState(0);
+
+    const handleWin = () => {
+        setScore((prevScore) => prevScore + 1);
+    };
+
     return (
         <div className='cont-container'> 
-         <h1 className='text-center p-2'>Rock Paper Scissors Game</h1>
-            <Board />
-            <Console />
+            <h1 className='text-center p-2'>Rock Paper Scissors Game</h1>
+            <Board score={score} />
+            <Console onWin={handleWin} />
         </div>
     );
 };
